f47929d2189a7bcbc88eff1ce737b85b786e1a6a
filipenevola <filipenevola@gmail.com>
2021-10-08 17:49:42 -0400

    Published simple:rest-method-mixin@1.1.0.

--- packages/rest-method-mixin/README.md
+++ packages/rest-method-mixin/README.md
@@ -1,3 +1,13 @@
+# Compatibility
+
+**Compatible with Meteor 2.4**
+
+This repository provides versions for the package [simple:rest-method-mixin](https://github.com/meteor-compat/meteor-rest/tree/devel/packages/rest-method-mixin) that are compatible with latest Meteor. This is necessary because the author is not maintaining package anymore.
+
+## Changes
+- v1.1.0
+    - `api.versionsFrom` on `Package.onUse` was changed from `1.2.1` to `2.4`.
+
 # simple:rest-method-mixin
 
 A mixin for [`mdg:validated-method`](https://atmospherejs.com/mdg/validated-method) to configure the HTTP endpoint for that Method when using [`simple:rest`](https://github.com/stubailo/meteor-rest/blob/master/packages/rest/README.md).
--- packages/rest-method-mixin/package.js
+++ packages/rest-method-mixin/package.js
@@ -1,19 +1,19 @@
 Package.describe({
   name: 'simple:rest-method-mixin',
-  version: '1.0.1',
+  version: '1.1.0',
   // Brief, one-line summary of the package.
   summary: 'Mixin for simple:rest with ValidatedMethod',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/stubailo/meteor-rest',
+  git: 'https://github.com/meteor-compat/meteor-rest',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
+  api.versionsFrom('2.4');
   api.use('ecmascript');
-  api.use('simple:rest@1.1.1');
+  api.use('simple:rest@1.2.0');
   api.addFiles('rest-method-mixin.js');
   api.export('RestMethodMixin');
 });
